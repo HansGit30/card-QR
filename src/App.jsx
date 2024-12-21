38c1248092f6d1d36e29ab80d7f1552533294d0d
HansGit30 <homgit07@gmail.com>
2024-12-20 23:22:45 -0500

    card

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Codigoqr from './components/Codigoqr'
+
+function App() {
+
+  return (
+    <>
+      <section className='w-80 h-auto bg-white rounded-2xl py-5'>
+        <Codigoqr />
+      </section>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Codigoqr.jsx
@@ -0,0 +1,16 @@
+
+const Codigoqr = () => {
+  return (
+    <>
+      <div className="w-full h-auto px-5">
+        <img src="image-qr-code.png" className="rounded-2xl" alt="" />
+      </div>
+      <div className="mt-5">
+        <h2 className="text-xl font-bold px-6">Improve your front-end skills by building projects</h2>
+        <p className="p-5">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+      </div>
+    </>
+  )
+}
+
+export default Codigoqr
